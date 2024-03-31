628fb91d1c0ad61df89593b473ee8ab4f547f8a6
Esteban <estebanospinasaldarriaga@gmail.com>
2024-03-30 21:04:56 -0500

    front end header

--- resources/js/Components/Header.vue
+++ resources/js/Components/Header.vue
@@ -1,12 +1,20 @@
 <template>
     <header class="bg-gray-800 text-white py-4 px-8 flex justify-between items-center">
-        <h1 class="text-lg font-bold">Bluesoft Bank</h1>
-        <p>{{ userEmail }}</p>
+        <div class="flex text-lg font-bold">
+            <Link href="/dashboard" class="ml-4 hover:underline cursor-pointer">Bluesoft Bank</Link>
+            <Link href="/create-account" class="ml-4 hover:underline cursor-pointer">Create Account</Link>
+            <Link href="/transfer" class="ml-4 hover:underline cursor-pointer">Transfer</Link>
+            <Link href="/recent-transactions" class="ml-4 hover:underline cursor-pointer">Recent Transactions</Link>
+        </div>
+        <div>
+            <p>{{ userEmail }}</p>
+        </div>
     </header>
 </template>
 
 <script setup>
 import { defineProps } from 'vue';
+import { Link } from '@inertiajs/vue3'
 
 const props = defineProps({
     userEmail: {
