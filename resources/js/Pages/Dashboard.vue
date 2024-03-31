f34dbff3079a9d5f4276d6ba19f0cbb1162607ff
Esteban <estebanospinasaldarriaga@gmail.com>
2024-03-30 21:05:10 -0500

    update dashboard to display list of accounts

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,22 +1,42 @@
-<script setup>
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import { Head } from '@inertiajs/vue3';
-</script>
-
 <template>
-    <Head title="Dashboard" />
-
-    <AuthenticatedLayout>
-        <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Dashboard</h2>
-        </template>
-
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900 dark:text-gray-100">You're logged in!</div>
-                </div>
+    <Header :userEmail="userEmail" />
+    <div class="container mx-auto p-4">
+        <h1 class="text-2xl font-bold mb-4">Account Dashboard</h1>
+        <div v-if="accounts.length === 0" class="text-gray-600">No accounts found.</div>
+        <div v-else>
+            <div v-for="account in accounts" :key="account.id" class="border rounded p-4 my-2">
+                <p class="text-lg font-bold">ID: {{ account.id }}</p>
+                <p class="">Type: {{ account.account_type }}</p>
+                <p>Balance: ${{ account.balance }}</p>
+                <p v-if="account.city">City: {{ account.city }}</p> <!-- Display city if available -->
+                <p>Created At: {{ account.created_at }}</p>
+                
+                <button @click="generateExtracts(account.id)" class="bg-blue-500 text-white font-bold py-2 px-4 rounded mt-2">Generate Statements</button>
             </div>
         </div>
-    </AuthenticatedLayout>
+    </div>
 </template>
+
+<script setup>
+import Header from '@/Components/Header.vue';
+import { router } from '@inertiajs/vue3'
+
+// Props
+const props = defineProps({
+    accounts: {
+        type: Array,
+        required: true
+    },
+    userEmail: {
+        type: String,
+        required: true
+    }
+})
+
+const generateExtracts = (accountId) => {
+    const queryParams = {
+        accountId: accountId
+    }
+    router.get('/generate-statements/', queryParams)
+}
+</script>
