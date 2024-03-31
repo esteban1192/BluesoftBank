89da36277667c58d345f436bf5d9baeced65f653
Esteban <estebanospinasaldarriaga@gmail.com>
2024-03-30 21:03:52 -0500

    added relationships to transaction model

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -5,8 +5,33 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\User;
+use App\Models\Account;
 
 class Transaction extends Model
 {
     use HasFactory;
+
+    /**
+     * Get the user associated with the transaction.
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Get the account where the transaction is from.
+     */
+    public function fromAccount()
+    {
+        return $this->belongsTo(Account::class, 'from_account_id');
+    }
+
+    /**
+     * Get the account where the transaction is to.
+     */
+    public function toAccount()
+    {
+        return $this->belongsTo(Account::class, 'to_account_id');
+    }
 }
